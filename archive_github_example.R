50d7fee92fa5ac6524fe274bc3dcb0723ea5e2c2
Kathryn Doering <kathryn.doering@noaa.gov>
2022-07-18 11:57:51 -0400

    first attempt at setting up batch file

--- archive_github_example.R
+++ archive_github_example.R
@@ -1,29 +1,39 @@
-# example R script for archiving github repos using googledrive
+# Archive github repos using googledrive -----
 # call from powershell using rscript archive_github_example.R
+
+# setup -----
+# manual changes
+org <- "r4ss" # could change to other github org names.
+
+#install.packages("googledrive")
 #devtools::install_github("ropensci-org/gitcellar")
+library(gitcellar)
+library(googledrive)
+
+# use an auto auth setting so no interaction is needed
+# See gargle's "Non-interactive auth" vignette for more details:
+# https://gargle.r-lib.org/articles/non-interactive-auth.html
+options(gargle_oauth_email = "*@noaa.gov")
+
+# Download locally ----
 local_dest_folder <- "download_archives"
-gitcellar::download_organization_repos("r4ss", dest_fold = local_dest_folder) # or other org
+gitcellar::download_organization_repos(org, dest_fold = local_dest_folder)
 
+# upload to gdrive ----
 files_to_upload <- list.files(local_dest_folder, recursive = TRUE)
-
 lapply(files_to_upload, function(archive_file, local_dest_folder) {
   local_file_path <- file.path(local_dest_folder, archive_file)
   g_drive_path <- googledrive::as_id("https://drive.google.com/drive/u/0/folders/1Qbs9ts4cQ9XlJ2mj6ZQAMSR4dAFQSTup")
-  g_drive_name <- basename(archive_file) # may want to append a date
+  g_drive_name <- basename(archive_file)
   # upload to google drive (expect needing some authentification, will be taken to browser)
   # drive put will either make a new file if one doesnt exist, or update the
   # version if one already exists.
   googledrive::drive_put( media = local_file_path,  path = g_drive_path, 
   name = g_drive_name)
 }, local_dest_folder = local_dest_folder)
-
-# cleanup
+ 
+# cleanup ----
 unlink(local_dest_folder, recursive = TRUE)
-
-# might need to check on the auth, to make sure it is available when scheduling 
-# a job. One resource:
-#   See gargle's "Non-interactive auth" vignette for more details:
-# https://gargle.r-lib.org/articles/non-interactive-auth.html
 
 # Finally, need to set this up to run with windows task scheduler. May need to 
 # simply make a batch file in the same location as this script that contains
---
+++ run_archive_github.bat
@@ -0,0 +1,2 @@
+cd C:\Users\Kathryn.Doering\Documents\github_repos\test_github_site
+rscript archive_github_example.R
